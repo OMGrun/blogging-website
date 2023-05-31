cebf813328e7aee2dfca1652a2cc57d36faa16b7
OMGrun <niphitphon.saint.sachdev@gmail.com>
2023-06-01 06:13:22 +0700

    accessing post title from home.ejs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -17,7 +18,8 @@ app.use(express.static("public"));
 
 app.get('/', (req,res) =>
 {
-  res.render(__dirname + "/views/home.ejs", {StartingContent : homeStartingContent})
+  res.render(__dirname + "/views/home.ejs", {StartingContent : homeStartingContent, post : posts})
+
 
 })
 
@@ -39,6 +41,8 @@ app.get('/compose', (req,res) =>
 app.post('/compose',(req,res) =>
 {
   const post = {postTitle : req.body.postTitle, postBody : req.body.postBody};
+  posts.push(post);
+  res.redirect('/')
 
 })
 
